ccc9436807e002c93d4411482ead0941500b0482
Thom van der Ven <thomvanderven@gmail.com>
2026-05-25 17:15:28 +0200

    made a cool homepage

---
+++ main.js
@@ -0,0 +1,44 @@
+const sources = [
+	"tsxpl1.jpg",
+	"tsxpl2.jpg",
+	"tsxpl3.jpg",
+	"tsxpl4.jpg"
+];
+
+const viewer = document.getElementById("coverimage");
+const bottomImage = document.getElementById("bottomImage");
+const topImage = document.getElementById("topImage");
+
+function update(progress) {
+	const segments = sources.length - 1;
+
+	// Convert 0→1 into segment space
+	const scaled = progress * segments;
+
+	// Current pair
+	const index = Math.min(
+		Math.floor(scaled),
+		segments - 1
+	);
+
+	// Blend amount inside current pair
+	const mix = scaled - index;
+
+	bottomImage.src = sources[index];
+	topImage.src = sources[index + 1];
+
+	viewer.style.setProperty("--mix", mix);
+}
+
+viewer.addEventListener("mousemove", (e) => {
+	const rect = viewer.getBoundingClientRect();
+
+	let progress =
+		(e.clientX - rect.left) / rect.width;
+
+	progress = Math.max(0, Math.min(1, progress));
+
+	update(progress);
+});
+
+update(0);
